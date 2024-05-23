c35bd5a267db0647cc81712798f5415400467b12
enghddd <wkdalsrud113@naver.com>
2024-05-23 17:55:07 +0900

    readme update

--- django_project/movies/serializers.py
+++ django_project/movies/serializers.py
@@ -61,7 +61,7 @@ class MovieSerializer(serializers.ModelSerializer):
         class Meta:
             model = Actor
             fields = ('actor_name',)
-
+    
     similars = MovieListSerializer(many=True, read_only=True)
     review_count = serializers.IntegerField(source='review_set.count', read_only=True)
     like_count = serializers.IntegerField(source='user_like_movie.count', read_only=True)
